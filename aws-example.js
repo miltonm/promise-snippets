aa4ecb8fe0377fd0f53e08c46d1ac4f8e94cb232
miltonm <miltonm@users.noreply.github.com>
2015-08-26 14:40:21 +0100

    Create aws-example.js

---
+++ aws-example.js
@@ -0,0 +1,19 @@
+//http://stackoverflow.com/questions/28953211/promisify-aws-sdk-via-bluebird
+var SES = new Promise.promisifyAll(new AWS.SES());
+
+SES.listVerifiedEmailAddressesAsync().then(function (err,emails) {
+    console.log('p',err,emails);
+});
+
+
+// http://stackoverflow.com/questions/26475486/how-do-i-promisify-the-aws-javascript-sdk
+var Promise = require("bluebird");
+
+var AWS = require('aws-sdk');
+var dynamoDbConfig = {
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  region: process.env.AWS_REGION
+};
+var dynamoDb = new AWS.DynamoDB(dynamoDbConfig);
+Promise.promisifyAll(Object.getPrototypeOf(dynamoDb));
