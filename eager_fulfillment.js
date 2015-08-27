ad7f48014ab969b6112d0da200bb156ae571b79f
miltonm <miltonm@users.noreply.github.com>
2015-08-27 14:10:33 +0100

    Update eager_fulfillment.js

--- eager_fulfillment.js
+++ eager_fulfillment.js
@@ -1,7 +1,22 @@
-// Do promises get fulfilled eagerly or lazily ? My hypothesis if eager
+// Do promises get fulfilled eagerly or lazily ? 
 // So there is this function that gives me a timer promise of 10 seconds
 // At time=t I get the promise
 // Wait for 15 seconds. Now time is t+15.
 // Do a then on timer promise 
-// When will the then get invoked at t+15 or t+15+10 ?
+// When will the then get invoked at t+15 or t+15+10 ? 
+// Ans: t+15+10
 
+var Promise = require('bluebird');
+var _ = require('lodash');
+var f = function() { return Promise.delay(5000); }
+var t1,t2,t3; t1= _.now(); 
+setTimeout(
+  function() { 
+    f().then(function() {
+      t3 = _.now();
+    }); 
+    t2 = _.now()
+  }, 
+  5000);
+console.log(t2 - t1);
+console.log(t3 - t1);
